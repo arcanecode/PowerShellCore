b3d6c614c1f3dac879f444c2a4389cd2df4accc9
ArcaneCode <arcanecode@gmail.com>
2019-03-11 16:34:56 -0500

    Testing for caching

--- Demo/test.ps1
+++ Demo/test.ps1
@@ -4,3 +4,4 @@ Get-ChildItem
 # HEre is another comment.
 # Now another whacked out comment
 # Yet another comment to test credential caching. Pre enter of them
+# OK now testing to see if they cached.
