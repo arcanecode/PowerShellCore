2f02d6c18f21a654989580890bcab60db0d2d8c9
ArcaneCode <arcanecode@gmail.com>
2019-03-11 22:08:01 -0500

    Yet another test

--- Demo/test.ps1
+++ Demo/test.ps1
@@ -2,6 +2,3 @@
 Get-ChildItem
 
 # HEre is another comment.
-# Now another whacked out comment
-# Yet another comment to test credential caching. Pre enter of them
-# OK now testing to see if they cached.
