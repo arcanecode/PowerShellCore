364816cf9f4a078a40f67d0b3acc765f5b7cdd67
ArcaneCode <arcanecode@ArcaneMac13.domain>
2019-04-02 10:35:21 -0500

    Began work on Azure Demo

--- Demo/m11-cool-things-2-azure.ps1
+++ Demo/m11-cool-things-2-azure.ps1
@@ -14,3 +14,17 @@
   written consent of the author and/or Pluralsight. It can be used within
   your own projects.
 -----------------------------------------------------------------------------#>
+
+# The Azure module can be found in the PowerShell Gallery. The PSGallery
+# can be thought of as a repository. 
+
+# First verify it is in the gallery and see its version
+Find-Module Az
+
+# See if the module is already on the system
+Get-Module Az* -ListAvailable  
+
+# Install the module
+Install-Module Az
+
+
